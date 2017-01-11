564a2e7abe6c9cccf0d49a5f0242e83351ce0780
Esther Low <phi21416@gmail.com>
2017-01-11 09:04:11 +0800

    completed i think

--- models/todo.js
+++ models/todo.js
@@ -3,24 +3,87 @@ const uuidGenerator = require('uuid/v4')
 let todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
-function create (params) {
+function create (name, description = '', completed = false) {
   // uuid usage exmaple
   // let uuid = uuidGenerator()
+  var instanceOfTodo = {}
+  instanceOfTodo.uuid = uuidGenerator();
+  instanceOfTodo.id = (todos.length + 1)
+    if (name === undefined) {
+      console.log('name cannot be undefined');
+      return false; }
+    instanceOfTodo.name = name;
+    instanceOfTodo.description = description
+    instanceOfTodo.completed = completed;
+  todos.push(instanceOfTodo)
+  console.log(todos);
+  return true
 }
+/*
+{_id:###,
+name: '',
+description: ''
+completed: false}
+*/
 
 // READ (list & show)
 function list () {
-  return todos
+console.log(typeof todos);
+return todos
 }
-function show (id) {}
+
+
+function show (id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      //console.log(todos[i]);
+      return todos[i]
+    }
+  }
+  //console.log('null');
+  return null
+}
 
 // UPDATE - params should be an object with KVPs for the fields to update
-function update (id, updatedParams) {}
+function update (id, updatedParams) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      for (var key in updatedParams) {
+      if (key === 'name') {
+           if ((Object.values(updatedParams).toString().length) < 5 ) {
+             //console.log((Object.values(updatedParams).toString().length));
+             console.log("Name is not allowed to be empty or have less than 5 characters");
+             return false;
+              }
+           }
+         }
+      todos[i] = Object.assign({}, todos[i], updatedParams)
+      console.log(todos[i]);
+      return true;
+    }
+
+  }
+  console.log('id not found');
+  return false;
+}
+
+
 
 // DESTROY (destroy & destroyAll)
-function destroy (id) {}
+function destroy (id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id) {
+      todos.splice(i, 1);
+      console.log("item id: " + id + " has been deleted." );
+      return todos.length;
+    }
+  }
+}
+
 function destroyAll () {
-  return true
+  todos = [];
+  console.log('everything deleted');
+  return todos.length
 }
 
 module.exports = {
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -2,6 +2,17 @@ const assert = require('assert')
 const todos = require('../models/todo.js')
 
 // Example Test - we expect that when we run destroyAll, it should return true to let us know it was successful
-assert.strictEqual( todos.destroyAll() , true, 'DestroyAll should return true, to indicate success')
 // We also expect the list should now be empty
-assert.strictEqual( todos.list().length , 0, 'List should be empty after deleting all TODOs')
+//assert.strictEqual(todos.list().length, 0, 'List should be empty after deleting all TODOs')
+// expect todos.create to return true if successful
+assert.strictEqual(todos.create('case1', 'trying', true), true, 'Create should return true to indicate success.')
+assert.strictEqual(todos.create('case2'), true, 'Create should return true to indicate success.')
+//assert.strictEqual(todos.create(), false, 'Create should return false to indicate failure.')
+//assert.strictEqual((typeof todos.list()),'object' , 'List should return "objects" for typeof todos')
+//assert.strictEqual((typeof todos.show(2)), 'object', 'Show should return a object')
+//assert.strictEqual(todos.update(2, {description: 'this should be updated'}), true, 'Update should allow param to be updated.')
+//assert.strictEqual(todos.update(2, {description: 'updated this again', completed: true}), true, 'Update should allow param to be updated.')
+//assert.strictEqual(todos.update(3, {name: 'case3'}), false, 'Id should not exist, hence update should fail.')
+//assert.strictEqual(todos.update(2, {name: ''}), false, 'Names should not be blank or less than 5 charaters')
+//assert.strictEqual(todos.destroy(2), 1, 'todos should only have 1 item left in the array.')
+assert.strictEqual(todos.destroyAll(), 0, 'DestroyAll should return 0, to indicate success')
